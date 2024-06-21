8d9d370264f667c1f12162e9ce65a591feca14ba
Sanjay <sanjayasd45@gmail.com>
2024-06-21 16:46:12 +0530

    Sdf

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const { Builder, By, until } = require('selenium-webdriver');
 require('chromedriver');
 const cors = require("cors")
+require('dotenv').config();
 
 
 const app = express();
